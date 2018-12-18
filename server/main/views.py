8ffcfda93a9355ad33088ab202813cfed3b17c8e
HDGgSom <mohoho2@mail.ru>
2018-12-18 16:01:31 +0300

    Второе дз

--- server/main/views.py
+++ server/main/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
+from django.template import Template, Context
+from django.http import HttpResponse
+from django.template.loader import get_template
 
 def main_view(request):
-    return render(request, 'main/index.html')
+
+    return render(
+        request, 
+        'main/index.html',
+        {
+            'page_title': 'Магазин',
+        }
+    )
 
 def contact_view(request):
-    return render(request, 'main/contact.html')
+
+    return render(
+        request, 
+        'main/contact.html',
+        {
+            'page_title': 'Контакты',
+        }
+    )
 
 def products_view(request):
-    return render(request, 'main/products.html')
+    return render(
+        request,
+        'main/products.html',
+        {
+            'page_title': 'Каталог',
+        }
+    )
